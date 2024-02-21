55c38539c3c05c3adcc7658f981cb4086044cb4f
Marce Calle <marcelo.callejo@gmail.com>
2024-02-21 12:24:19 -0300

    introduccion proyecto react

--- clase-2/src/App.jsx
+++ clase-2/src/App.jsx
@@ -1,33 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const estilos = {
+    width: '350px',
+    border: '3px solid white',
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Proyecto React 0.1</h1>
+      <img style={estilos} src="https://elcomercio.pe/resizer/dQTfmB6zr99dI9z0D9CpmwzLBAo=/1200x900/smart/filters:format(jpeg):quality(90)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/MSYW7AOXHRD3FBUH4HFP7SS2F4.jpg" alt="Deamon slayer" />
     </>
   )
 }
