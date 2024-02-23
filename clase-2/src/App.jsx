1638dee1553e3833781ecefee0cfe9c853e97d2a
marcecalle <38355063+marcecalle@users.noreply.github.com>
2024-02-23 16:05:05 -0300

    Estilos h1 y h2

--- clase-2/src/App.jsx
+++ clase-2/src/App.jsx
@@ -2,15 +2,22 @@ import './App.css'
 
 function App() {
 
-  const estilos = {
+  const estilosImg = {
     width: '350px',
     border: '3px solid white',
   };
+  const estilosTituloPrincipal = {
+    marginBottom: '5px'
+  }
+  const estilosTituloSecundario = {
+    marginTop: '5px'
+  }
 
   return (
     <>
-      <h1>Proyecto React 0.1</h1>
-      <img style={estilos} src="https://elcomercio.pe/resizer/dQTfmB6zr99dI9z0D9CpmwzLBAo=/1200x900/smart/filters:format(jpeg):quality(90)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/MSYW7AOXHRD3FBUH4HFP7SS2F4.jpg" alt="Deamon slayer" />
+      <h1 style={estilosTituloPrincipal}>CLASE 2</h1>
+      <h3 style={estilosTituloSecundario} >Proyecto React 0.1</h3>
+      <img style={estilosImg} src="https://elcomercio.pe/resizer/dQTfmB6zr99dI9z0D9CpmwzLBAo=/1200x900/smart/filters:format(jpeg):quality(90)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/MSYW7AOXHRD3FBUH4HFP7SS2F4.jpg" alt="Deamon slayer" />
     </>
   )
 }
